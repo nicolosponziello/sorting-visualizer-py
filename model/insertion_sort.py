1d29243b6311ecd9ade660f6ba381d7ba4e53a90
nicolosponziello <nicolosponziello@gmail.com>
2021-01-20 12:33:38 +0100

    color elements when sorting

--- model/insertion_sort.py
+++ model/insertion_sort.py
@@ -7,7 +7,8 @@ def insertion_sort(array, update_callback):
         while j >= 0 and array[j] > key:
             array[j+1] = array[j]
             j -= 1
-            update_callback(array, [])
+            update_callback(array, ['yellow' if x == j else 'red' if x == i else 'lightblue' for x in range(len(array))])
         array[j+1] = key
-        time.sleep(1)
+        update_callback(array, ['yellow' if x == j else 'red' if x == i else 'lightblue' for x in range(len(array))])
+        time.sleep(0.5)
 

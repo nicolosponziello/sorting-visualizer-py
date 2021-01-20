177cfec00b2093ae47ad07f7c23a6adad3229c6f
nicolosponziello <nicolosponziello@gmail.com>
2021-01-20 22:59:57 +0100

    update view layout

--- view/view.py
+++ view/view.py
@@ -1,3 +1,4 @@
+from tkinter.constants import HORIZONTAL, LEFT
 from algorithms import Algorithms
 import tkinter as tk
 
@@ -7,40 +8,60 @@ class View():
         self.controller = None
         self.width = width
         self.height = height
-        self.canvas_width = width - 200
-        self.canvas_height = height - 220
+        self.canvas_width = width - 0.2 * width
+        self.canvas_height = height - 0.3 * height
+
+    def generateAndDraw(self, size, min, max):
+        self.controller.generateNewArray(size, min, max)
+        self.draw_array(self.controller.getArray())
 
     def init(self, controller):
         self.root = tk.Tk()
         self.root.title('Sorting Visualizer')
         self.root.geometry(str(self.width) + 'x' + str(self.height))
+        
         if controller == None:
             raise Exception('controller cannot be none')
         self.controller = controller
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
+        self.frame = tk.Frame(self.root, relief=tk.RAISED)
+        self.frame.pack(fill=tk.BOTH, expand=True)
+
+        self.input_frame = tk.Frame(self.frame)
+        self.input_frame.pack()
     
-        debug_print_button = tk.Button(self.frame, text="print", command= lambda: print(self.controller.getArray()))
-        debug_print_button.pack(side=tk.LEFT)
-        generate_button = tk.Button(self.frame, text="Generate", command=lambda: self.controller.generateNewArray(elem_num.get(), min_value_entry.get(), self.max_value_entry.get()))
+        #buttons 
+        generate_button = tk.Button(self.input_frame, text="Generate", command=lambda: self.generateAndDraw(elem_num.get(), min_value_entry.get(), self.max_value_entry.get()))
         generate_button.pack(side=tk.LEFT)
-        draw_button = tk.Button(self.frame, text="Draw", command=lambda: self.draw_array(self.controller.getArray()))
-        draw_button.pack(side=tk.LEFT)
+        
         algo = tk.StringVar(self.root)
         algo.set(algo_options[0])
-        sort_button = tk.Button(self.frame, text="Sort",command=lambda: self.controller.sort(algo.get()))
+        
+        sort_button = tk.Button(self.input_frame, text="Sort",command=lambda: self.controller.sort(algo.get()))
         sort_button.pack(side=tk.LEFT)
-        algo_menu = tk.OptionMenu(self.frame, algo, *algo_options)
-        algo_menu.pack(side=tk.RIGHT)
-        elem_num = tk.Scale(self.root, from_=10, to=1000)
-        elem_num.pack()
-        min_value_entry = tk.Entry(self.root)
-        min_value_entry.pack()
-        self.max_value_entry = tk.Entry(self.root)
-        self.max_value_entry.pack()
+        
+        #menu
+        algo_menu = tk.OptionMenu(self.input_frame, algo, *algo_options)
+        algo_menu.pack(side=LEFT)
+        
+        #elem config
+        elem_num = tk.Scale(self.input_frame, from_=10, to=150, orient=tk.HORIZONTAL, label="Elements:", length=200)
+        elem_num.pack(side=LEFT)
+       
+        min_value_label = tk.Label(self.input_frame, text="Min Value:")
+        min_value_label.pack(side=LEFT)
+        min_value_entry = tk.Entry(self.input_frame)
+        min_value_entry.pack(side=LEFT)
+       
+        max_value_label = tk.Label(self.input_frame, text="Max Value: ")
+        max_value_label.pack(side=LEFT)
+        self.max_value_entry = tk.Entry(self.input_frame)
+        self.max_value_entry.pack(side=LEFT)
+
         min_value_entry.insert(tk.END, 10)
         self.max_value_entry.insert(tk.END, 100)
-        self.canvas = tk.Canvas(self.root, width=self.canvas_width, height=self.canvas_height)
+
+        #canvas
+        self.canvas = tk.Canvas(self.frame, width=self.canvas_width, height=self.canvas_height)
         self.canvas.pack()
 
     def show(self):

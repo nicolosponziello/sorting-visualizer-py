c079614201ba5847a5c2219c251a8fc280ad828d
nicolosponziello <nicolosponziello@gmail.com>
2021-01-23 21:19:11 +0100

    fix insertion_sort impl

--- model/insertion_sort.py
+++ model/insertion_sort.py
@@ -1,14 +1,13 @@
 import time
 
-def insertion_sort(array, update_callback):
-    for i in range(1, len(array)):
+def insertion_sort(array, update_callback, delay=0.01):
+    for i in range(0, len(array)):
         key = array[i]
         j = i - 1
         while j >= 0 and array[j] > key:
             array[j+1] = array[j]
             j -= 1
             update_callback(array, ['yellow' if x == j else 'red' if x == i else 'lightblue' for x in range(len(array))])
+            time.sleep(delay)
         array[j+1] = key
-        update_callback(array, ['yellow' if x == j else 'red' if x == i else 'lightblue' for x in range(len(array))])
-        time.sleep(0.5)
 

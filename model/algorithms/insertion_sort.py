a50b3497cf991230cb101ada4a57fb86df146165
nicolosponziello <nicolosponziello@gmail.com>
2021-01-24 13:29:16 +0100

    insertion_sort: make rects green when done

--- model/algorithms/insertion_sort.py
+++ model/algorithms/insertion_sort.py
@@ -1,13 +1,14 @@
 import time
 
-def insertion_sort(array, update_callback, delay=0.01):
+def insertion_sort(array, update_func, delay=0.01):
     for i in range(0, len(array)):
         key = array[i]
         j = i - 1
         while j >= 0 and array[j] > key:
             array[j+1] = array[j]
             j -= 1
-            update_callback(array, ['yellow' if x == j else 'red' if x == i else 'lightblue' for x in range(len(array))])
+            update_func(array, ['yellow' if x == j else 'red' if x == i else 'lightblue' for x in range(len(array))])
             time.sleep(delay)
         array[j+1] = key
+    update_func(array, ['lightgreen' for _ in range(len(array))])
 

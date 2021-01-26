2af62f1766911dd44555c954575f9e3ad26511a3
nicolosponziello <nicolosponziello@gmail.com>
2021-01-26 18:25:44 +0100

    tests: check if array are not equals before sorting

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -16,41 +16,48 @@ class TestAlgorithms(unittest.TestCase):
     def test_insertion_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         insertion_sort(array, test_func, 0)
         self.assertEqual(sorted_arr, array)
     
     def test_bubble_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         bubble_sort(array, test_func, 0)
         self.assertEqual(sorted_arr, array)
     
     def test_counting_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         counting_sort(array, test_func, 0)
         self.assertEqual(sorted_arr, array)
     
     def test_heap_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         heap_sort(array, test_func, 0)
         self.assertEqual(sorted_arr, array)
     
     def test_merge_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         merge_sort(array, test_func, 0)
         self.assertEqual(sorted_arr, array)
     
     def test_quick_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         quicksort(array, 0, len(array)-1, test_func, 0)
         self.assertEqual(sorted_arr, array)
     
     def test_selection_sort(self):
         array = [random.randint(10, 100) for _ in range(100)]
         sorted_arr = sorted(array)
+        self.assertNotEqual(array, sorted_arr)
         selection_sort(array, test_func, 0)
         self.assertEqual(sorted_arr, array)
